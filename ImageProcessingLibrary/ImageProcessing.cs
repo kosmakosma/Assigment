911dcff125d8e5710c5a4d1c8561123e336ec6ff
Mateusz Małecki <mmateusz314@gmail.com>
2019-06-07 17:26:13 +0200

    Add project files.

---
+++ ImageProcessingLibrary/ImageProcessing.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcessingLibrary
+{
+    enum PrimaryColor
+    {
+        Red, Blue, Green
+    }
+
+    public class ImageProcessing
+    {
+
+        public ImageProcessing(Image image)
+        {
+            Image = new Bitmap(image);
+        }
+
+        public ImageProcessing(string filepath)
+        {
+            LoadImage(filepath);
+        }
+
+        public ImageProcessing()
+        {
+
+        }
+
+        public void LoadImage(string filepath)
+        {
+            Image = new Bitmap(filepath);
+        }
+
+        public void SaveImage(string filepath)
+        {
+            Image.Save(filepath);
+        }
+
+        public Bitmap Image { get; set; }
+
+        public int Width
+        {
+            get { return Image.Width; }
+        }
+
+        public int Height
+        {
+            get { return Image.Height; }
+        }
+
+        public Bitmap ToMainColors()
+        {
+
+            BitmapData from = Image.LockBits(new Rectangle(0, 0, Width, Height), ImageLockMode.ReadWrite, Image.PixelFormat);
+            unsafe
+            {
+                int bytesPerPixel = Bitmap.GetPixelFormatSize(Image.PixelFormat) / 8;
+                int heightInPixels = from.Height;
+                int widthInPixels = from.Width;
+
+                byte* firstPixel = (byte*)from.Scan0;
+
+
+                Parallel.For(0, heightInPixels, y =>
+                {
+                    byte* currentLine = firstPixel + (y * from.Stride);
+
+                    for (int x = 0; x < widthInPixels * bytesPerPixel; x += bytesPerPixel)
+                    {
+                        switch (GetMaxFromRGB(currentLine[x + 2], currentLine[x + 1], currentLine[x]))
+                        {
+                            case PrimaryColor.Red:
+                                currentLine[x] = 0;
+                                currentLine[x + 1] = 0;
+                                currentLine[x + 2] = 0xFF;
+                                break;
+                            case PrimaryColor.Green:
+                                currentLine[x] = 0;
+                                currentLine[x + 1] = 0xFF;
+                                currentLine[x + 2] = 0;
+                                break;
+                            case PrimaryColor.Blue:
+                                currentLine[x] = 0xFF;
+                                currentLine[x + 1] = 0;
+                                currentLine[x + 2] = 0;
+                                break;
+                        }
+                    }
+
+                });
+
+            }
+            Image.UnlockBits(from);
+
+            return Image;
+        }
+
+        private PrimaryColor GetMaxFromRGB(int r, int g, int b)
+        {
+            if (r >= Math.Max(g, b))
+                return PrimaryColor.Red;
+            else if (g >= Math.Max(r, b))
+                return PrimaryColor.Green;
+            else return PrimaryColor.Blue;
+        }
+
+        public async Task<Bitmap> ToMainColorsAsync()
+        {
+            return await Task.Run(() =>
+            {
+                return ToMainColors();
+            });
+        }
+    }
+}
